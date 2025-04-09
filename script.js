7be2530d28878afc2bd590da6f56a1a72aa4012e
Paulo Almeida <145488386+Phsla00@users.noreply.github.com>
2025-04-09 17:52:12 -0300

    feat: add human choice

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function getComputerChoice(){
     //Sortear um valor aleatorio e guardar em uma variavel
     let randomNumber = Math.floor(Math.random() * 3);
-    //retornar essa variavel
+    //Retornar essa variavel
     return randomNumber;
 }
 
-
+function getHumanChoice(){
+    //Pedir a escolha do jogador
+    let number = prompt("Escolha:\nRock = 0\nPaper = 1\nScissors = 2");
+    //Retornar a escolha do jogador
+    return number;
+}
