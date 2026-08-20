ea936f237d44cc64838c9ac7a032772fbf60ffaa
Paulo Almeida <145488386+Phsla00@users.noreply.github.com>
2026-08-20 15:34:34 -0300

    Add a feature to get the human's choice

--- script.js
+++ script.js
@@ -2,14 +2,26 @@ function getComputerChoice(){
     const randomNumber = Math.floor(Math.random() * 3);
     let computerChoice;
 
-    if (randomNumber === 0){
+    if(randomNumber === 0){
         computerChoice = 'Rock';
-    } else if (randomNumber === 1){
+    }else if(randomNumber === 1){
         computerChoice = 'Paper'
-    } else {
+    }else {
         computerChoice = 'Scissors'
     }
     
     return computerChoice;
 }
 
+function getHumanChoice(){
+    let humanChoice = +prompt("Escolha sua jogada: 1. Rock! 2. Paper! 3. Scissors!"); 
+    if(humanChoice === 1){
+        humanChoice = 'Rock';
+    }else if(humanChoice === 2){
+        humanChoice = 'Paper';
+    }else{
+        humanChoice = 'Scissors';
+    }
+    
+    return humanChoice;
+}
