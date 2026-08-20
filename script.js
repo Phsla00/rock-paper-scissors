433259ea65928d3706de507e3c757e33792016eb
Paulo Almeida <145488386+Phsla00@users.noreply.github.com>
2026-08-20 15:16:55 -0300

    Add a feature to get the computer's choice

---
+++ script.js
@@ -0,0 +1,15 @@
+function getComputerChoice(){
+    const randomNumber = Math.floor(Math.random() * 3);
+    let computerChoice;
+
+    if (randomNumber === 0){
+        computerChoice = 'Rock';
+    } else if (randomNumber === 1){
+        computerChoice = 'Paper'
+    } else {
+        computerChoice = 'Scissors'
+    }
+    
+    return computerChoice;
+}
+
