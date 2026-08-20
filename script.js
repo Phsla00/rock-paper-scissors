bf042efa97e7b68ae3a6162e6ae0fa51f39a560e
Paulo Almeida <145488386+Phsla00@users.noreply.github.com>
2026-08-20 17:13:18 -0300

    Add feacture to play 5 rounds and show the winner

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function getComputerChoice(){
     }else {
         computerChoice = 'scissors'
     }
-    
+
     return computerChoice;
 }
 
@@ -26,41 +26,60 @@ function getHumanChoice(){
     return humanChoice;
 }
 
-let humanScore = 0;
-let computerScore = 0;
 
-function playRound(humanChoice, computerChoice){
-    let humanChoiceLow = humanChoice.toLowerCase();
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
     
-    if(humanChoiceLow === computerChoice){
-        console.log(`It's a tie! You both chose ${humanChoice}`)
+    function playRound(humanChoice, computerChoice){
+        let humanChoiceLow = humanChoice.toLowerCase();
+        
+        if(humanChoiceLow === computerChoice){
+            console.log(`It's a tie! You both chose ${humanChoice}`)
+        }
+        
+        if( humanChoiceLow === 'rock'){
+            if(computerChoice === 'paper'){
+                console.log('You lose! Paper beats Rock.');
+                computerScore += 1;
+            }else if(computerChoice === 'scissors'){
+                console.log('You win! Rock beats Scissors.');
+                humanScore += 1;
+            }    
+        }else if(humanChoiceLow === 'paper'){
+            if(computerChoice === 'rock'){
+                console.log('You win! Paper beats Rock.');
+                humanScore += 1;
+            }else if(computerChoice === 'scissors'){
+                console.log('You lose! Scissors beats Paper');
+                computerScore += 1;
+            }
+        }else if(humanChoiceLow === 'scissors'){
+            if(computerChoice === 'rock'){
+                console.log('You lose! Rock beats Scissors.');
+                computerScore += 1;
+            } else if(computerChoice === 'paper'){
+                console.log('You win! Scissors beats Paper.');
+                humanScore += 1;
+            }
+        }
+    }
+
+    function showWinner(){
+        if(humanScore > computerScore){
+            console.log(`You win the match! ${humanScore} X ${computerScore}.`);
+        }else if(humanScore < computerScore){
+            console.log(`You lose the match! ${humanScore} X ${computerScore}`);
+        }else{
+            console.log('It\'s a tie!');
+        }
+    }
+
+    for(i = 0; i < 5; i++){
+        playRound(getHumanChoice(), getComputerChoice());
     }
     
-    if( humanChoiceLow === 'rock'){
-        if(computerChoice === 'paper'){
-            console.log('You lose! Paper beats Rock.');
-            computerScore += 1;
-        }else if(computerChoice === 'scissors'){
-            console.log('You win! Rock beats Scissors.');
-            humanScore += 1;
-        }    
-    }else if(humanChoiceLow === 'paper'){
-        if(computerChoice === 'rock'){
-            console.log('You win! Paper beats Rock.');
-            humanScore += 1;
-        }else if(computerChoice === 'scissors'){
-            console.log('You lose! Scissors beats Paper');
-            computerScore += 1;
-        }
-    }else if(humanChoiceLow === 'scissors'){
-        if(computerChoice === 'rock'){
-            console.log('You lose! Rock beats Scissors.');
-            computerScore += 1;
-        } else if(computerChoice === 'paper'){
-            console.log('You win! Scissors beats Paper.');
-            humanScore += 1;
-        }
-    }
+    showWinner()
 }
 
-playRound(getHumanChoice(), getComputerChoice());
+playGame();
